e23bb098cf4f118ccd8eed3ac234743f5b2f9f49
rndy28 <rendyramadhan838@gmail.com>
2022-07-17 09:40:30 +0700

    add normalize.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
+import "normalize.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "App";
+import type {} from "styled-components/cssprop";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
